6dd348ec792abd699bfa90833e7adb3776a6ae8c
Regina Traber <traber.regina@hispeed.ch>
2023-11-25 20:27:09 +0100

    ex7

--- Ch07/Exercises.hs
+++ Ch07/Exercises.hs
@@ -17,8 +17,7 @@ import Prelude hiding (any, takeWhile, dropWhile, {- map, filter, -} curry, uncu
 -- using the higher-order functions map and filter.
 
 e_7_1 :: (a -> b) -> (a -> Bool) -> [a] -> [b]
--- e_7_1 f p xs = ???map???filter???
-e_7_1 = {- TODO -} undefined
+e_7_1 f p xs = map f (filter p xs)
 
 
 ------------------------------------------------------------------------
@@ -35,28 +34,40 @@ e_7_1 = {- TODO -} undefined
 --     all :: (a -> Bool) -> [a] -> Bool
 
 all :: (a -> Bool) -> [a] -> Bool
-all = {- TODO -} undefined
+all p = and . map p
+--all p = foldr (&&) True . map p
 
 
 -- (b) Decide if any element of a list satisfies a predicate:
 --     any :: (a -> Bool) -> [a] -> Bool
 
 any :: (a -> Bool) -> [a] -> Bool
-any = {- TODO -} undefined
+any p = or . map p
+--any p = foldr (||) False . map p
 
 
 -- (c) Select elements from a list while they satisfy a predicate:
 --     takeWhile :: (a -> Bool) -> [a] -> [a]
 
 takeWhile :: (a -> Bool) -> [a] -> [a]
-takeWhile = {- TODO -} undefined
+takeWhile p = foldr (\x -> if p x then (x:) else const []) []
+--takeWhile _ [] = []
+--takeWhile p (x:xs)
+--    | p x = x : takeWhile p xs
+--    | otherwise = []
+-- Rmk: not the same as takeWhile p = filter p
 
 
 -- (d) Remove elements from a list while they satisfy a predicate:
 --     dropWhile :: (a -> Bool) -> [a] -> [a]
 
 dropWhile :: (a -> Bool) -> [a] -> [a]
-dropWhile = {- TODO -} undefined
+dropWhile _ [] = []
+dropWhile p (x:xs)
+    | p x = dropWhile p xs
+    | otherwise = x:xs
+-- Rmk: didn't find a way to use foldr
+-- Rmk: not the same as dropWhile p = filter (not . p)
 
 
 ------------------------------------------------------------------------
@@ -67,13 +78,18 @@ dropWhile = {- TODO -} undefined
 -- instead of 'map' and 'myFilter' instead of 'filter'.
 
 myMap :: (a -> b) -> [a] -> [b]
--- myMap f = foldr ??? ???
-myMap = {- TODO -} undefined
+--myMap f = foldr (\x xs -> f x : xs) []
+--myMap f = foldr ((:) . f) []
+--myMap f = foldr (\x -> (:) (f x)) []
+myMap f = foldr ((++) . (\x -> [f x])) []
+
 
 
 myFilter :: (a -> Bool) -> [a] -> [a]
--- myFilter f = foldr ??? ???
-myFilter = {- TODO -} undefined
+--myFilter p = foldr (\x xs -> if p x then x:xs else xs) []
+--myFilter p = foldr ((++) . (\x -> if p x then [x] else [])) []
+--myFilter p = foldr (\x -> if p x then (x:) else id) []
+myFilter p = foldr (\x -> (++) (if p x then [x] else [])) []
 
 
 
@@ -87,9 +103,43 @@ myFilter = {- TODO -} undefined
 -- curried function with two arguments into a function on pairs.
 -- Hint: first write down the types of the two functions.
 
--- curry :: TODO
-curry f = {- TODO -} undefined
+curry :: ((a,b) -> c) -> (a -> b -> c)
+curry f = \x y -> f (x,y)
 
 
--- uncurry :: TODO
-uncurry f = {- TODO -} undefined
+uncurry :: (a -> b -> c) -> ((a,b) -> c)
+uncurry f = \(x,y) -> f x y
+
+
+{-
+  `curry` and `uncurry` functions provide a concise way to convert between curried and uncurried forms of functions.
+
+  Use `curry` to partially apply arguments, creating specialized versions of functions, and for elegant function composition.
+
+  f :: Int -> Int -> Int
+  g :: Int -> Int
+  h :: Int
+
+  -- curry example
+  f' :: Int -> (Int -> Int)
+  g' :: Int -> Int
+  h' :: Int
+
+  f' = curry f
+  g' = f' 5  -- g' is a partially applied version of f'
+  h' = g' 10 -- h' is the result of applying g' to another argument
+
+  Use `uncurry` to adapt functions for compatibility with libraries or situations where an uncurried form is more convenient.
+
+  -- uncurry example
+  f :: Int -> Int -> Int
+  g :: (Int, Int) -> Int
+
+  -- Convert curried function to uncurried
+  g = uncurry f
+
+  result = g (5, 10) -- Using the uncurried function with a tuple
+-}
+
+
+
